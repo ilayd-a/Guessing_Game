1e0d69fccbc761db1cccf7af320d1ee417f90371
ilayd <id2275@nyu.edu>
2025-01-03 16:57:53 +0300

    working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io; // input/output library
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    println!("Please input your guess.");
+    let mut guess = String::new();
+    // we use let statement to create the variable
+    // in rust, variables are immutable by default, to make a variable mutable we add the keyword
+    // mut before the variable name
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    println!("You guessed: {}", guess);
 }
