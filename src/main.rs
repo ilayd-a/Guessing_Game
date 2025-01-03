952f4ec61d2116a25d3a2539975d02e01c394c2e
ilayd <id2275@nyu.edu>
2025-01-03 17:22:08 +0300

    getting the input done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+// by ilayd January 3 20255
 use std::io; // input/output library
 
 fn main() {
@@ -7,8 +8,12 @@ fn main() {
     // we use let statement to create the variable
     // in rust, variables are immutable by default, to make a variable mutable we add the keyword
     // mut before the variable name
+    // guess is a new mutable string variable
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
+    // if we hadn't imported the io library with use std::io, at the beginning of the program, we
+    // could still use the function by writing this function call as std::io::stdin
     println!("You guessed: {}", guess);
+    // curly braces are placeholders for variables
 }
