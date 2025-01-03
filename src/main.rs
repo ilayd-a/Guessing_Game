b44f05b6be6461b143b995ff9273516956f9a73f
ilayd <id2275@nyu.edu>
2025-01-03 23:03:00 +0300

    handling invalid input

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ use std::cmp::Ordering;
 fn main() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("The secret number is: {}", secret_number);
     loop {
         println!("Please input your guess.");
         let mut guess = String::new();
@@ -19,7 +18,10 @@ fn main() {
             .expect("Failed to read line");
         // if we hadn't imported the io library with use std::io, at the beginning of the program, we
         // could still use the function by writing this function call as std::io::stdin
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
         // rust allows us to shadow the previous value of guess with a new one
         // the parse method will only work on characters that can logically be converted into numbers
         println!("You guessed: {}", guess);
